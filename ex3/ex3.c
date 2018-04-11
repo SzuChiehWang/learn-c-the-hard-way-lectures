a020aa27c2b8f9dabd71e7f407d951599f7a53cb
Kate <scwang1123@gmail.com>
2018-04-11 23:14:55 +0800

    Formatted Printing

---
+++ ex3/ex3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    int age=10;
+    int height=3;
+    printf("i am %d years old\n",age);
+    printf("i am %d inches tall\n", height);
+
+    return 0;
+}
